5f63dc0d14d59fa879bf4d0d1a7cf4d328da8db6
Oaksoe25 <oaksoezaw211120@gmail.com>
2025-10-13 12:12:13 +0700

    week-5

--- week-5/string.js
+++ week-5/string.js
@@ -1,11 +1,10 @@
 // string.js
 function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  
-  function reverse(str) {
-    return str.split('').reverse().join('');
-  }
-  
-  module.exports = { capitalize, reverse };
-  
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+module.exports = { capitalize, reverse };
