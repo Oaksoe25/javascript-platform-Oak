dd678fd40fecf889e144627442e288a030a83ec7
Oaksoe25 <oak.soe_ti24@nusaputra.ac.id>
2025-11-06 15:15:39 +0700

    chore: set start script and engines.node 20.x

--- week-7/OakTech-store-express/src/server.js
+++ week-7/OakTech-store-express/src/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();                 // ← create app FIRST
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // --- middleware ---
 app.use(express.json());
